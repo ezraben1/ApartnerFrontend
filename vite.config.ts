2eb7fa8a4f52e314bd3f7f9bd84ef2ba9524e6b9
Ben Ezra <118681190+ezraben1@users.noreply.github.com>
2023-05-11 17:33:06 +0300

    Prepare for Heroku deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/my-app/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/', // <-- Adjusted this line
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     open: true, // default: false
